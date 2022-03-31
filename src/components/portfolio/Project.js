647a8984e8c234a3ef22f268a6a5ac4b917f16b3
Dylan <dylan77120@live.fr>
2022-03-31 13:52:41 +0200

    debut du composant project

---
+++ src/components/portfolio/Project.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Project extends Component {
+    render() {
+        return (
+            <div>
+                Projet
+            </div>
+        );
+    }
+}
+
+export default Project;
--- src/pages/PortFolio.js
+++ src/pages/PortFolio.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import ProjectList from '../components/portfolio/ProjectList';
 
 const CV = () => {
     return (
-        <div>
+        <div className='portfolio'>
             <Navigation />
-            <h1>Hello depuis CV</h1>     
+            <ProjectList /> 
         </div>
     );
 };
