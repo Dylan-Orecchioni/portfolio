fb23a4d64e3d08a67684cdb68ff76416b0876364
Dylan <dylan77120@live.fr>
2022-06-23 18:04:33 +0200

    projet update 1

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -6,16 +6,16 @@ export const portfolioData = [
     languagesIcons: ['fab fa-symfony','fab fa-css3-alt'],
     source: 'https://dmstore.fr',
     info: 'Ceci est mon premier site web créer lors de ma formation chez Philiance Formation.',
-    picture: './media/project1.JPG'
+    picture: './media/dmstore.jpg'
   },
   {
     id: 2,
     name: 'zooplanet.com',
     languages: ['wordpress','css'],
     languagesIcons: ['fab fa-css3-alt','fab fa-wordpress'],
-    source: 'http://www.github.com',
+    source: 'https://zooplanet.go.yj.fr/',
     info: 'Mon deuxieme projet perso, cette fois ci sur Wordpress. J\'ai trouvé ca fun du coup je me suis lancé sur la creation d\' un site web sur Wordpress.',
-    picture: './media/project1.JPG'
+    picture: './media/zooplanet.jpg'
   },
 
 ]
